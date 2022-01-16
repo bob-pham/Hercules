df839acfac50fabdcca0dc493867d0f5913f7671
Bob Pham <bobbaopham@gmail.com>
2022-01-15 16:06:57 -0800

    implemented stats

--- app/src/main/java/com/example/hercules/model/User.java
+++ app/src/main/java/com/example/hercules/model/User.java
@@ -2,7 +2,9 @@ package com.example.hercules.model;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
     private Account account;
@@ -11,22 +13,27 @@ public class User {
     private double currentRewardsRemaining;
     private double totalRewardsGained;
     private List<Quest> quests;
+    private Set<User> friends;
+    private Stats userStats;
 
     public User(String id, String pw, String name, String birthDayString, String goalName, List<Quest> quests) {
         account = new Account(id, pw, name, birthDayString);
         goal.getGoal(Goal.GoalTypes.valueOf(goalName));
         this.quests = quests;
+        this.friends = new HashSet<>();
     }
 
     public User(String id, String pw, String name, String birthDayString, String goalName, Goal goal) {
         account = new Account(id, pw, name, birthDayString);
         this.quests = new ArrayList<>();
         this.goal = goal;
+        this.friends = new HashSet<>();
     }
 
     public User(String id, String pw, String name, String birthDayString) {
         account = new Account(id, pw, name, birthDayString);
         this.quests = new ArrayList<>();
+        this.friends = new HashSet<>();
     }
 
     public void addQuest(Quest quest) {
@@ -35,6 +42,14 @@ public class User {
 
     public void removeQuest(Quest quest) {
         this.quests.remove(quest);
+    }
+
+    public void addFriend(User friend) {
+        this.friends.add(friend);
+    }
+
+    public void removeFriend(User friend) {
+        this.friends.remove(friend);
     }
 
     public Account getAccount() {
