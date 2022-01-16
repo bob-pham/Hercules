f2e7e78bc65a41e03ce346b1480112f2f747c7f1
Bob Pham <bobbaopham@gmail.com>
2022-01-15 23:55:33 -0800

    dunno what i did checking for jay

--- app/src/main/java/com/example/hercules/view/ClosetFragment.java
+++ app/src/main/java/com/example/hercules/view/ClosetFragment.java
@@ -22,7 +22,7 @@ public class ClosetFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
