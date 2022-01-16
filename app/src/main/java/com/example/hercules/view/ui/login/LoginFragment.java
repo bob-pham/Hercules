40fd517503f10c5fbc805ba7b5cd7469c80dce2a
Bob Pham <bobbaopham@gmail.com>
2022-01-16 06:49:43 -0800

    i forgot what i did last

--- app/src/main/java/com/example/hercules/view/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/hercules/view/ui/login/LoginFragment.java
@@ -21,8 +21,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.hercules.view.R;
-import com.example.hercules.view.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
